e3af21fd86f9bf203735a674f4adef09884e8e6a
LAUBIE <maxime.laubie@eivp-paris.fr>
2020-11-21 12:54:20 +0100

    3

--- horaires/anomalies lumiere.py
+++ horaires/anomalies lumiere.py
@@ -1,30 +1,34 @@
 from variables.variables import LUM, temps_tries
-from time import localtime, mktime, strptime
+from time import mktime, strptime, localtime
+import datetime
 from fonctions_utiles.fonctions_selections import selectionneur_de_date
 import matplotlib.pyplot as plt
 
 
-def transfo_heure(l):
-
-    a = str(localtime(l).tm_hour) + ":" + str(localtime(l).tm_min) + ":" + str(localtime(l).tm_sec)
-    if len(a)<8:
-        if a[0] > 2:
-            a=str(0)+a
-    return mktime(strptime("1970-01-01 "+a, "%Y-%m-%d %H:%M:%S"))
-
+def transfo_heure(nbre):
+    b = localtime(nbre)
+    print(b)
+    a = datetime.datetime(b.tm_year, b.tm_mon, b.tm_mday, b.tm_hour, b.tm_min, b.tm_sec)
+    print(a)
+    print("1970-01-01 " + str(a.time()))
+    print(strptime("1970-01-01 " + str(a.time()), "%Y-%m-%d %H:%M:%S"))
+    return mktime(strptime("1970-01-01 " + str(a.time()), "%Y-%m-%d %H:%M:%S"))
+print(transfo_heure(temps_tries[274]))
+print(transfo_heure(temps_tries[275]))
 
 def heure_max_extinction_bureau():
     horaire_max = []
-    for k in selectionneur_de_date("2019-08-11 00:00:00", "2019-08-22 00:00:00")[1]:
-        if LUM[k] != 0 and LUM[k+1] == 0:
-            horaire_max.append(transfo_heure(temps_tries[k+1]))
+    for i in selectionneur_de_date("2019-08-11 00:00:00", "2019-08-22 00:00:00")[1]:
+        if LUM[i] != 0 and LUM[i + 1] == 0:
+            horaire_max.append(transfo_heure(temps_tries[i + 1]))
     return max(horaire_max)
+
 
 def heure_min_ouverture_bureau():
     horaire_min = []
-    for k in selectionneur_de_date("2019-08-11 00:00:00", "2019-08-22 00:00:00")[1]:
-        if LUM[k] == 0 and LUM[k+1] != 0:
-            horaire_min.append(transfo_heure(temps_tries[k + 1]))
+    for i in selectionneur_de_date("2019-08-11 00:00:00", "2019-08-22 00:00:00")[1]:
+        if LUM[i] == 0 and LUM[i + 1] != 0:
+            horaire_min.append(transfo_heure(temps_tries[i + 1]))
     return min(horaire_min)
 
 
@@ -32,26 +36,25 @@ def oublie_lumiere(l):
     indices_incorrectes = []
     a = heure_max_extinction_bureau()
     b = heure_min_ouverture_bureau()
-    temps_heure=[]
+    temps_heure = []
 
-    for k in range(len(temps_tries)):
-        temps_heure. append((transfo_heure(temps_tries[k])))
-        print(k)
-        print(temps_heure[k])
+    for i in range(len(temps_tries)):
+        temps_heure. append((transfo_heure(temps_tries[i])))
         print(len(temps_heure))
+        if temps_heure[0] > a or temps_heure[0] < b:
 
-    print(len(temps_heure))
+            l[i] = 0
+            indices_incorrectes.append(i)
+
     return indices_incorrectes, l
+print(oublie_lumiere(LUM))
 
-
-"""
-tps_corrig=[]
+lumiere_corrigee = oublie_lumiere(LUM)[1]
+tps_corrig = []
 for k in oublie_lumiere(LUM)[0]:
     tps_corrig.append(temps_tries[k])
 
-plt.plot(tps_corrig,LUM)
-plt.scatter(tps_corrig,lumiere_corrigee,"color=red")
-plt.show()"""
-
-plt.plot([1,2,3],[1,3,2])
+plt.plot(temps_tries, LUM)
+plt.scatter(tps_corrig, lumiere_corrigee, "color=red")
 plt.show()
+
