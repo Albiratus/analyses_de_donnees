2fe4ace7a8b9f614f6286440da65f7ea6a26af75
jeanne <jeanne.charron@eivp-paris.fr>
2020-11-21 15:14:05 +0100

    21/11/2020

--- Jeanne/Fonctions de bases.py
+++ Jeanne/Fonctions de bases.py
@@ -1,3 +1,41 @@
+import matplotlib.pyplot as plt
+
+import csv
+with open ('projet.csv', 'r') as f :
+    reader = csv.reader(f, delimiter =';')
+    donnees = []
+    for row in reader :
+        donnees.append(row)
+
+
+bruit = []
+temp = []
+humidite = []
+lum = []
+CO2 = []
+date = []
+
+i = 0
+for row in donnees :
+    if i != 0 :
+        bruit.append(row[2])
+        temp.append(row[3])
+        humidite.append(row[4])
+        lum.append(row[5])
+        CO2.append(row[6])
+        date.append(row[7][0:19])
+    i = i + 1
+
+from datetime import datetime
+
+from time import mktime, strptime, localtime
+datensec = []
+for k in date :
+    datensec.append(mktime(strptime (k, '%Y-%m-%d %H:%M:%S')))
+
+
+
+
 
 def minimum (liste):
     mini = liste [0]
@@ -16,7 +54,7 @@ def maximum (liste):
 def somme (liste):
     _somme = 0
     for i in liste :
-        _somme = _somme + i
+        _somme = _somme + float(i)
     return _somme
 
 def moyenne (liste):
@@ -32,16 +70,11 @@ def tri_rapide (liste):
         l1,l2 = [], []
         for x in liste :
             if x<pivot:
-                l1.append(x)
+                return l1.append(x)
             else:
-                l2.append(x)
+                return l2.append(x)
     return tri_rapide(l1)+[pivot]+tri_rapide(l2)
 
-
-'''def tri_insertion (liste):
-    for i in range (1, len(liste)):
-        val = liste[i]
-        pos = i'''
 
 
 def mediane (l):
@@ -59,7 +92,7 @@ def mediane (l):
 def variance (liste):
     a = moyenne(liste)
     n = len (liste)
-    b = ((x - a)**2 for x in liste)
+    b = ((float(x) - a)**2 for x in liste)
     var = somme (b)/n
     return var
 
@@ -67,9 +100,9 @@ def covariance (X, Y):
     multiplication = []
     # supposons que len(X) = len(Y)
     for k in range (0, len(X)):
-        xmoinsespx = X[k]- moyenne(X)
-        ymoinsespy = Y[k]- moyenne(Y)
-        multiplication.append(xmoinsespx[k]*ymoinsespy[k])
+        xmoinsespx = float(X[k])- moyenne(X)
+        ymoinsespy = float(Y[k])- moyenne(Y)
+        multiplication.append(xmoinsespx*ymoinsespy)
     return moyenne (multiplication)
 
 def correlation (X, Y):
@@ -78,89 +111,6 @@ def correlation (X, Y):
     c = covariance(X,Y)
     return c/((a*b)**(1/2))
 
-'''plt.scatter(temps, variable)
-plt.title ('courbe'+ str(k))
-plt.xlabel()
-plt.ylabel()'''
-
-
-'''plt.title("titre_graphique")
-plt.plot([1,2,4,6],[2,4,6,8], "g--", linewidth = 2)
-plt.xlabel("nom_axe_abscisses")
-plt.ylabel("nom_axe_ordonnés")
-# pour limiter la longueur du graphique
-plt.axis([borne 1 axe abscisse, borne 2, borne 1 axe ordonnées, borne 2])
-# pour montrer le graphique
-plt.show()'''
-
-
-import csv
-with open ('projet.csv', 'r') as f:
-    reader = csv.reader(f, delimiter =';')
-    donnees = []
-    for row in reader :
-        donnees.append(row)
-
-
-bruit = []
-temp = []
-humidite = []
-lum = []
-CO2 = []
-date = []
-
-
-for row in range (1, len(donnees)) :
-        bruit.append(donnees[row][2])
-        temp.append(donnees[row][3])
-        humidite.append(donnees[row][4])
-        lum.append(donnees[row][5])
-        CO2.append(donnees[row][6])
-        date.append(donnees[row][7][0:19])
-
-
-from datetime import datetime
-
-from time import mktime, strptime, localtime
-datensec = []
-for k in date :
-    datensec.append(mktime(strptime (k, '%Y-%m-%d %H:%M:%S')))
-
-print(strptime('2000:09:22 06:36:45', '%Y:%m:%d %H:%M:%S'))
-
-print(datetime.strptime('2000:09:22 06:36:45', '%Y:%m:%d %H:%M:%S').time())
-
-
-
-# t = temprérature en °C.
-# d = dew point (point de rosée) en °C.
-import math
-
-def alpha(t, Phi):
-    a = 17,27
-    b = 237,7
-    return a*t/(b + t) + math.log(Phi)
-
-def point_rosee (t, Phi):
-    return b*alpha(t, Phi)/(a - alpha(t, Phi))
-
-def recherche_indice (instant):
-# instant = une heure (ex : 2020-09-12 22:00:00)
-    for k in range (0, len(liste_date)):
-        if liste_date[k] == instant:
-            return k
-
-def humidex (instant):
-
-    kelvin = 273,5
-    temperature = t2[recherche_indice(instant)] + kelvin
-    dewpoint = point_rosee(t2[recherche_indice(instant)], humidité[recherche_indice(instant)]) + kelvin
-# calcul la pression de la vapeur en mbar
-    e = 6,11*math.exp(5417.7530*((1/kelvin)-(1/dewpoint)))
-# calcul la pression de la vapeur saturante
-    h = 0,5555*(e - 10)
-    humidex = temperature + h - kelvin
-    return humidex
 
 
 
@@ -175,7 +125,7 @@ def tri_insert (liste):
             a -= 1
     return liste, liste_indice
 
-print(tri_insert([6,5,4,3,2,1]))
+
 
 
 bruittri = []
@@ -183,18 +133,73 @@ temptri = []
 humiditetri = []
 lumtri = []
 CO2tri = []
-datensectri = tri_insert(datensec)
-print(datensectri)
+
+indice = tri_insert(datensec)[1]
+datensectri = tri_insert(datensec)[0]
 
 
-import matplotlib.pyplot as plt
+for k in indice :
+    bruittri.append(bruit[k])
+    temptri.append(temp[k])
+    humiditetri.append(humidite[k])
+    lumtri.append(lum[k])
+    CO2tri.append(CO2[k])
 
-plt.title("lumiere en fonction de la datensectri")
-plt.plot(datensectri, lum, "g--", linewidth = 2)
+
+
+'''plt.subplot(211)
+plt.title("bruittri en fonction de la datensectri")
+plt.plot(datensectri, bruittri, label = str(moyenne(bruit)), linewidth = 2)
 plt.xlabel("date en sec")
-plt.ylabel("intensité lumière")
-plt.legend('lumière')
-plt.plot(datensectri, bruit, "b--", linewidth = 2)
-plt.legend('bruit')
-# pour montrer le graphique
-plt.show()
+plt.ylabel("intensité bruit")
+plt.legend()
+plt.subplot(212)
+plt.plot(datensectri, temptri, "--b", linewidth = 2)
+plt.legend('temp')
+
+# plt.show()
+# pour montrer le graphique'''
+
+
+def traceur_de_correlation (X, Y):
+    plt.plot(datensectri, X, label = str(X))
+    plt.plot(datensectri, Y, label = str(Y))
+    plt.legend(str(correlation(X,Y)))
+    plt.xlabel('datensec')
+    plt.show()
+
+traceur_de_correlation (bruittri, temptri)
+
+
+
+# t = temprérature en °C.
+# d = dew point (point de rosée) en °C.
+import math
+
+def alpha(t, Phi):
+    a = 17,27
+    b = 237,7
+    return a*t/(b + t) + math.log(Phi)
+
+def point_rosee (t, Phi):
+    b = 237,7
+    a = 17,27
+    return b*alpha(t, Phi)/(a - alpha(t, Phi))
+
+def recherche_indice (instant):
+# instant = une heure (ex : 2020-09-12 22:00:00)
+    for k in range (0, len(datensectri)):
+        if datensectri[k] == instant:
+            return k
+
+def humidex (instant):
+
+    kelvin = 273,5
+    temperature = temptri[recherche_indice(instant)] + kelvin
+    dewpoint = point_rosee(temptri[recherche_indice(instant)], humiditetri[recherche_indice(instant)]) + kelvin
+# calcul la pression de la vapeur en mbar
+    e = 6,11*math.exp(5417.7530*((1/kelvin)-(1/dewpoint)))
+# calcul la pression de la vapeur saturante
+    h = 0,5555*(e - 10)
+    humidex = temperature + h - kelvin
+    return humidex
